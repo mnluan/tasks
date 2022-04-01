ac082197fb8bf53cb7951c4f6adc11494865c49e
Luan Nascimento <luann@id.uff.br>
2022-04-01 00:11:44 -0300

    Save tasks and delete

--- res/js/script.js
+++ res/js/script.js
@@ -1,4 +1,5 @@
 const $html = document.querySelector('html');
+var items = JSON.parse(localStorage.getItem('todo-list')) || [];
 var theme = false;
 
 function onClickMenu(){
@@ -47,7 +48,7 @@ function onClickMenu(){
     document.getElementById("menu").classList.toggle("change");
     document.getElementById("menu-bg").classList.toggle("change-bg");
   })
-
+  
 //Tasks menu
 function addTask() {
     document.getElementById('task-window').style.display = 'block';
@@ -58,11 +59,48 @@ function cancelTask() {
 }
 
 function newTask() {
-    var input = document.getElementById('task-name');
-    var text = input.value;
-    var time = date = new Date().toLocaleDateString();
-    var element = document.getElementById('tasklist-container');
-    element.innerHTML = '<div id="tasks" class="tasks">'+ text + '<br>' + time + '</div><br>';
-    document.getElementById('task-window').style.display = 'none';
+  let input = document.getElementById('task-name');
+  let text = input.value;
+  if (text === ""){
+    return alert("You need to put in a number");
+  }
+  items.push({
+    value: text, time: (new Date()).toLocaleDateString("en-US")
+  })
+
+  localStorage.setItem('todo-list', JSON.stringify(items));
+  listItems();
+
+  input.value = "";
+  let element = document.getElementById('tasklist-container');
+  //element.innerHTML = '<div id="tasks" class="tasks">'+ text + '<br>' + time + '<br><button onclick="update();" id="update"></button> </button> </div><br>';
+  document.getElementById('task-window').style.display = 'none';
+
 }
 
+function deleteItem(index) {
+  items.splice(index, 1);
+  localStorage.setItem('todo-list', JSON.stringify(items))
+  listItems();
+}
+
+function markAsDone(index) {
+  items[index].done = !items[index].done;
+  localStorage.setItem('todo-list', JSON.stringify(items));
+  listItems();
+}
+
+function listItems() {
+  var list = "";
+  for (var i = 0; i < items.length; i++) {
+    list += "<dt class"+ (items[i].done ? "done" : "") +">";
+    list += items[i].value + "<br>";
+    list += "<small class='label' onclick='markAsDone("+ i +")'>"+ items[i].time +"</small><br>";
+    list += "<span class='label alert' onclick='deleteItem("+ i +")'><button onclick='del();' id='delete'></span></dt><br>";
+  }
+  document.querySelector("#tasklist-container").innerHTML = list;
+}
+
+(function() {
+  listItems();
+})();
